3bb84bbd5e82a7a9f2d50f3bc1c089b028c3a791
Leonardo Salas <leonardo@we.hn>
2022-07-05 20:20:16 -0500

    gravity and aceleration

--- dasher.cpp
+++ dasher.cpp
@@ -1,9 +1,13 @@
 #include "raylib.h"
+#include <string>
+using namespace std;
 
 const int windowWidth{1280};
 const int windowHeigth{720};
 const Color backgroundColor{WHITE};
 const int targetFPS{60};
+const float gravityFroce{20}; // 9.8(m/s)/s
+const float gravityFrameUpdate{gravityFroce / targetFPS};
 
 int main()
 {
@@ -13,20 +17,42 @@ int main()
     //character
     const int width{50};
     const int height{80};
-    int posY{windowHeigth - height};
-    int velocity{0};
+    const int groundPos{windowHeigth - height};
+    const int jumpForce{10};
+
+    float posY{groundPos};
+    float velocity{0};
 
     SetTargetFPS(targetFPS);
     while( !WindowShouldClose() ){
         // start drawing
         BeginDrawing();
         ClearBackground(backgroundColor);
+        
+        // start game logic
+        if (posY >= groundPos)
+        {
+            //grounded
+            velocity = 0;
+        }                
+        else
+        {
+            //on air
+            velocity += gravityFrameUpdate;
+        }
+
+        if (IsKeyDown(KEY_SPACE))
+        {
+            velocity = -jumpForce;
+        }
 
         posY += velocity;
+        //end game logic
 
-        if(IsKeyDown(KEY_SPACE)){
-            velocity = -10;
-        }
+        //printf_s("velocity: %i \n", velocity);
+        //printf_s("posY: %i \n", posY);
+        //printf_s("groundPos: %i \n", groundPos);
+
         DrawRectangle((windowWidth / 2) - (width / 2), posY, width, height, BLUE);
 
         //end drawing
